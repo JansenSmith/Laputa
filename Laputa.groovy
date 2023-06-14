e4d2514531ab07d51a588cf579ee1af7120cf58e
JansenSmith <jansen@bstarengineering.com>
2023-06-14 04:29:21 -0400

    diagnozing embeddings

--- Laputa.groovy
+++ Laputa.groovy
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,14 +11,19 @@ import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine
 import javafx.scene.control.TextInputDialog
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 import groovy.json.JsonSlurper
+import org.apache.commons.codec.binary.Base64
 
 // inspired by https://simonwillison.net/2023/Jan/13/semantic-search-answers/
 
 public class QAPatternExample {
 
     public static void main(String[] args) throws IOException {
-        String question = "What does shot scraper do?";
+        String question = "What is a CSG file?";
+		
+		// Look for or save OpenAI API key
         String keyLocation = ScriptingEngine.getWorkspace().getAbsolutePath()+"/gpt-key.txt"
 		if(!new File(keyLocation).exists()) {
 			BowlerStudio.runLater({
@@ -68,12 +74,20 @@ public class QAPatternExample {
         String prompt = constructPrompt(searchResults, question);
         System.out.println("\nPrompt: \n" + prompt);
 
-        // Step 3: Call the OpenAI API with the prompt
-        String answer = OpenAIAPIClient.callDavinciAPI(prompt, apiKey);
+        // Step 3: Call the OpenAI API with embeddings
+        String[] inputs = ["input1", "input2", "input3"];
+
+        List<Float> embeddings = OpenAIAPIClient.callEmbeddingAPI(inputs, apiKey);
+
+        System.out.println("Embeddings:");
+        for (Float embedding : embeddings) {
+            System.out.println(embedding);
+        }
+//        String answer = OpenAIAPIClient.callDavinciAPI(prompt, apiKey);
 
         // Step 4: Process and display the answer
-        String processedAnswer = processAnswer(answer);
-        System.out.println("\nProcessed Answer: \n" + processedAnswer);
+//        String processedAnswer = processAnswer(answer);
+//        System.out.println("\nProcessed Answer: \n" + processedAnswer);
     }
 
     private static String runSearchQuery(String question) {
@@ -88,101 +102,6 @@ public class QAPatternExample {
         // Glue the extracted content together with the question to form the prompt
         return searchResults + "\n\nGiven the above content, answer the following question: " + question;
     }
-
-
-	
-	public class OpenAIAPIClient {
-		private static final String DAVINCI_API_URL = "https://api.openai.com/v1/engines/davinci/completions";
-		private static final String EMBEDDING_API_URL = "https://api.openai.com/v1/embeddings";
-	
-		private static String callOpenAIAPI(String url, String prompt, String apiKey) throws IOException {
-			HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-			connection.setRequestMethod("POST");
-			connection.setRequestProperty("Content-Type", "application/json");
-			connection.setRequestProperty("Authorization", "Bearer " + apiKey);
-			connection.setDoOutput(true);
-	
-			String data = "{\"prompt\": \"" + escapeNewLines(prompt) + "\"}";
-	
-			connection.getOutputStream().write(data.getBytes());
-	
-			int responseCode = connection.getResponseCode();
-	
-			BufferedReader reader;
-			if (responseCode >= 400) {
-				reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
-			} else {
-				reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			}
-	
-			StringBuilder response = new StringBuilder();
-			String line;
-			while ((line = reader.readLine()) != null) {
-				response.append(line);
-			}
-			reader.close();
-	
-			return response.toString();
-		}
-	
-		private static String escapeNewLines(String text) {
-			return text.replace("\n", "\\n");
-		}
-	
-		public static String callDavinciAPI(String prompt, String apiKey) throws IOException {
-			return callOpenAIAPI(DAVINCI_API_URL, prompt, apiKey);
-		}
-	
-		public static String callEmbeddingAPI(String prompt, String apiKey) throws IOException {
-			return callOpenAIAPI(EMBEDDING_API_URL, prompt, apiKey);
-		}
-	}
-	
-	
-//	private static String callOpenAIAPI(String prompt, String apiKey) throws IOException {
-//		URL url = new URL("https://api.openai.com/v1/engines/davinci/completions");
-//		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//		connection.setRequestMethod("POST");
-//		connection.setRequestProperty("Content-Type", "application/json");
-//		connection.setRequestProperty("Authorization", "Bearer " + apiKey);
-//		connection.setDoOutput(true);
-//		
-//		//String data = "{\"prompt\": \"" + prompt + "\", \"max_tokens\": 100}";
-//	    String promptWithEscapes = prompt.replace("\n", "\\n");
-//	    String data = "{\"prompt\": \"" + promptWithEscapes + "\"}";
-//	    //System.out.println("JSON Data: " + data);
-//	
-//	    // Validate the JSON
-//	    try {
-//	        new JsonSlurper().parseText(data);
-//	        //System.out.println("JSON is valid.");
-//	    } catch (Exception e) {
-//	        System.out.println("JSON is invalid. Error: " + e.getMessage());
-//	    }
-//	
-//	    connection.getOutputStream().write(data.getBytes());
-//		
-//	    int responseCode = connection.getResponseCode();
-//	    //System.out.println("Response Code: " + responseCode);
-//		
-//	    BufferedReader reader;
-//	    if (responseCode >= 400) {
-//	        reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
-//	    } else {
-//	        reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//	    }
-//
-//		StringBuilder response = new StringBuilder();
-//		String line;
-//		while ((line = reader.readLine()) != null) {
-//			response.append(line);
-//		}
-//		reader.close();
-//	
-//	    	//System.out.println("Response Body: " + response.toString());
-//
-//		return response.toString();
-//	}
 
 
 	private static String processAnswer(String answer) {
@@ -209,3 +128,145 @@ public class QAPatternExample {
 	}
 
 }
+
+
+
+public class OpenAIAPIClient {
+    private static final String DAVINCI_API_URL = "https://api.openai.com/v1/engines/davinci/completions";
+    private static final String EMBEDDING_API_URL = "https://api.openai.com/v1/embeddings";
+    private static final int EMBEDDING_SIZE = 1536;
+
+    private static String callOpenAIAPI(String url, String data, String apiKey) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("Authorization", "Bearer " + apiKey);
+        connection.setDoOutput(true);
+
+        connection.getOutputStream().write(data.getBytes());
+
+        int responseCode = connection.getResponseCode();
+
+        BufferedReader reader;
+        if (responseCode >= 400) {
+            reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+        } else {
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        }
+
+        StringBuilder response = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            response.append(line);
+        }
+        reader.close();
+
+        resp = response.toString()
+
+	    System.out.println("API Response:");
+	    System.out.println(resp);
+
+        return resp;
+    }
+
+    public static String callDavinciAPI(String prompt, String apiKey) throws IOException {
+        String data = "{\"prompt\": \"" + escapeNewLines(prompt) + "\"}";
+        return callOpenAIAPI(DAVINCI_API_URL, data, apiKey);
+    }
+
+    public static List<Float> callEmbeddingAPI(String[] inputs, String apiKey) throws IOException {
+        String data = "{\"input\": " + buildInputJson(inputs) + ", \"model\": \"text-embedding-ada-002\"}";
+        String response = callOpenAIAPI(EMBEDDING_API_URL, data, apiKey);
+
+        // Extract the binary string from the response
+        String binaryString = extractBinaryString(response);
+
+        // Decode the binary string to obtain the list of floating-point numbers
+        List<Float> embeddings = decodeBinaryString(binaryString);
+
+        return embeddings;
+    }
+
+    private static String escapeNewLines(String text) {
+        return text.replace("\n", "\\n");
+    }
+
+    private static String buildInputJson(String[] inputs) {
+        StringBuilder json = new StringBuilder("[");
+        for (int i = 0; i < inputs.length; i++) {
+            json.append("\"").append(inputs[i]).append("\"");
+            if (i != inputs.length - 1) {
+                json.append(", ");
+            }
+        }
+        json.append("]");
+        return json.toString();
+    }
+
+    private static String extractBinaryString(String response) {
+        // Extract the binary string from the API response
+        int startIndex = response.indexOf("\"embedding\": \"") + 14;
+        int endIndex = response.indexOf("\"", startIndex);
+        return response.substring(startIndex, endIndex);
+    }
+
+	private static List<Float> decodeBinaryString(String binaryString) {
+	    byte[] binaryData = binaryString.decodeBase64()
+	    ByteBuffer buffer = ByteBuffer.wrap(binaryData)
+	    buffer.order(ByteOrder.LITTLE_ENDIAN)
+	
+	    List<Float> embeddings = []
+	    while (buffer.hasRemaining()) {
+	        embeddings.add(buffer.getFloat())
+	    }
+	    return embeddings
+	}
+}
+
+	
+//public class OpenAIAPIClient {
+//	private static final String DAVINCI_API_URL = "https://api.openai.com/v1/engines/davinci/completions";
+//	private static final String EMBEDDING_API_URL = "https://api.openai.com/v1/embeddings";
+//
+//	private static String callOpenAIAPI(String url, String prompt, String apiKey) throws IOException {
+//		HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+//		connection.setRequestMethod("POST");
+//		connection.setRequestProperty("Content-Type", "application/json");
+//		connection.setRequestProperty("Authorization", "Bearer " + apiKey);
+//		connection.setDoOutput(true);
+//
+//		String data = "{\"prompt\": \"" + escapeNewLines(prompt) + "\"}";
+//
+//		connection.getOutputStream().write(data.getBytes());
+//
+//		int responseCode = connection.getResponseCode();
+//
+//		BufferedReader reader;
+//		if (responseCode >= 400) {
+//			reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+//		} else {
+//			reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//		}
+//
+//		StringBuilder response = new StringBuilder();
+//		String line;
+//		while ((line = reader.readLine()) != null) {
+//			response.append(line);
+//		}
+//		reader.close();
+//
+//		return response.toString();
+//	}
+//
+//	private static String escapeNewLines(String text) {
+//		return text.replace("\n", "\\n");
+//	}
+//
+//	public static String callDavinciAPI(String prompt, String apiKey) throws IOException {
+//		return callOpenAIAPI(DAVINCI_API_URL, prompt, apiKey);
+//	}
+//
+//	public static String callEmbeddingAPI(String prompt, String apiKey) throws IOException {
+//		return callOpenAIAPI(EMBEDDING_API_URL, prompt, apiKey);
+//	}
+//}
