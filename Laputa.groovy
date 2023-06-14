a311a68042cbbb8e9374fe05d04e77d777ed3fdf
JansenSmith <jansen@bstarengineering.com>
2023-06-14 01:52:51 -0400

    Ignore the project files

--- Laputa.groovy
+++ Laputa.groovy
@@ -1,1 +1,67 @@
-// code here
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class QAPatternExample {
+
+    public static void main(String[] args) throws IOException {
+        String question = "What does shot scraper do?";
+        String apiKey = "YOUR_OPENAI_API_KEY";
+
+        // Step 1: Run a search query to find relevant content
+        String searchResults = runSearchQuery(question);
+
+        // Step 2: Extract relevant content and construct the prompt
+        String prompt = constructPrompt(searchResults, question);
+
+        // Step 3: Call the OpenAI API with the prompt
+        String answer = callOpenAIAPI(prompt, apiKey);
+
+        // Step 4: Process and display the answer
+        String processedAnswer = processAnswer(answer);
+        System.out.println("Answer: " + processedAnswer);
+    }
+
+    private static String runSearchQuery(String question) {
+        // Implement your search query logic here
+        // Use the question to search for relevant content in your documentation
+        // Return the search results as a string
+        return "Search results for the question: " + question;
+    }
+
+    private static String constructPrompt(String searchResults, String question) {
+        // Extract relevant content from search results
+        // Glue the extracted content together with the question to form the prompt
+        return searchResults + "\n\nGiven the above content, answer the following question: " + question;
+    }
+
+    private static String callOpenAIAPI(String prompt, String apiKey) throws IOException {
+        URL url = new URL("https://api.openai.com/v1/engines/davinci-codex/completions");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("Authorization", "Bearer " + apiKey);
+        connection.setDoOutput(true);
+
+        String data = "{\"prompt\": \"" + prompt + "\", \"max_tokens\": 100}";
+        connection.getOutputStream().write(data.getBytes());
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        StringBuilder response = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            response.append(line);
+        }
+        reader.close();
+
+        return response.toString();
+    }
+
+    private static String processAnswer(String answer) {
+        // Extract and process the answer from the API response
+        // Return the processed answer as a string
+        return "Processed answer: " + answer;
+    }
+}
